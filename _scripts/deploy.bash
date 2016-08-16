b6ceaf99a86dc274a7f38b12d70ed4ec0e1c6806
po_taka <angel.koilov@gmail.com>
2016-08-16 21:58:38 +0300

    deploy script fixes

--- _scripts/deploy.bash
+++ _scripts/deploy.bash
@@ -1,4 +1,4 @@
-DEPLOY_IP="#server host/ip"
+DEPLOY_IP="Server ip/hostname"
 DEPLOY_USER=ubuntu
 APP_PATH="/var/www/latestnews"
 DEPLOY_PATH_LIVE="${APP_PATH}/latest";
@@ -14,9 +14,10 @@ ssh ${DEPLOY_USER}@$DEPLOY_IP "if [ -d ${DEPLOY_PATH_RELEASE} ]; then rm ${DEPLO
   composer install -d ${DEPLOY_PATH_RELEASE} --no-dev &&
   rm ${DEPLOY_PATH_RELEASE}/app/config/parameters.yml &&
   ln -s ${APP_PATH}/shared/app/config/parameters.yml ${DEPLOY_PATH_RELEASE}/app/config/parameters.yml &&
-  ln -s ${APP_PATH}/shared/var/cache/prod/user_cache ${DEPLOY_PATH_RELEASE}/var/cache/prod/user_cache
   sudo chown www-data:www-data -R ${DEPLOY_PATH_RELEASE} &&
   if [ -d ${DEPLOY_PATH_LIVE} ]; then sudo rm ${DEPLOY_PATH_LIVE} -rf ; fi &&
   sudo mv ${DEPLOY_PATH_RELEASE} ${DEPLOY_PATH_LIVE} &&
-  sudo -u www-data ${DEPLOY_PATH_LIVE}/bin/console cache:clear --env=prod
+  sudo -u www-data ${DEPLOY_PATH_LIVE}/bin/console cache:clear --env=prod &&
+  sudo ln -s ${APP_PATH}/shared/var/cache/prod/user_cache ${DEPLOY_PATH_LIVE}/var/cache/prod/user_cache &&
+  sudo chown www-data:www-data -R ${DEPLOY_PATH_LIVE}
 "
